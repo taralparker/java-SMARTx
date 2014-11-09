34b7c7ec78c96d060e21cdb3040cd3f24d2e40e7
jamylyn <jamy_lyn@hotmail.com>
2014-11-08 19:10:44 -0600

    Text To Voice
    Start of text to voice function

--- SMARTx/app/src/main/java/com/test/smartx/Gates_was_here.java
+++ SMARTx/app/src/main/java/com/test/smartx/Gates_was_here.java
@@ -3,18 +3,48 @@ package com.test.smartx;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.HashMap;
 
 
-public class Gates_was_here extends Activity {
+public class Gates_was_here extends Activity implements TextToSpeech.OnInitListener, TextToSpeech.OnUtteranceCompletedListener {
+
+    TextToSpeech tts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gates_was_here);
         setTitle("SMARTx");
+
+        tts = new TextToSpeech(Gates_was_here.this, Gates_was_here.this );
+        final Button speak = (Button) findViewById(R.id.button2);
+        //final TextView tv = (TextView) findViewById(R.id.textView);
+        final String s = "I like it when you touch me there";
+
+        speak.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v){
+                if(!tts.isSpeaking()){
+                    HashMap<String, String> params = new HashMap<String, String>();
+                    params.put(TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID, "SampleText");
+                    //tts.speak(tv.getText().toString(), TextToSpeech.QUEUE_ADD, params);
+                    tts.speak(s, TextToSpeech.QUEUE_ADD, params);
+                    speak.setVisibility(Button.GONE);
+                } else {
+                    tts.stop();
+                }
+            }
+        });
+
     }
 
     public void buttonOnClick(View v)
@@ -41,5 +71,32 @@ public class Gates_was_here extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onInit(int i) {
+        tts.setOnUtteranceCompletedListener(this);
+    }
+
+    @Override
+    public void onUtteranceCompleted(String s) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                //Toast.makeText(Gates_was_here.this, "Utterence complete", Toast.LENGTH_SHORT).show();
+                Button btn = (Button) findViewById(R.id.button2);
+                btn.setVisibility(Button.VISIBLE);
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy(){
+        if(tts!=null){
+            tts.stop();
+            tts.shutdown();
+            tts = null;
+        }
+        super.onDestroy();
     }
 }
